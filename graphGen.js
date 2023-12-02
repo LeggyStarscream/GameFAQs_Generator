99554713b65047a27391ebdbb69bad71aeebf252
LeggyStarscream <leggystarscream@gmail.com>
2023-12-02 18:16:32 -0500

    Base Commit for posterity

---
+++ graphGen.js
@@ -0,0 +1,22 @@
+let a = "A";
+var g = new graphlib.Graph();
+g.setNode("A");
+
+var raw_grammar = {
+	"name": ["Arjun","Yuuma","Darcy","Mia","Chiaki","Izzi","Azra","Lina"],
+	"animal": ["unicorn","raven","sparrow","scorpion","coyote","eagle","owl","lizard","zebra","duck","kitten"],
+	"mood": ["vexed","indignant","impassioned","wistful","astute","courteous"],
+	"story": ["#hero# traveled with her pet #heroPet#.  #hero# was never #mood#, for the #heroPet# was always too #mood#."],
+	"origin": ["#[hero:#name#][heroPet:#animal#]story#"]
+};
+
+function generate()
+{
+    var grammar = tracery.createGrammar(raw_grammar);
+    const element = document.getElementById("Output");
+    element.innerText = a;
+    if (g.hasNode("A"))
+    {
+        a = a + " " + grammar.flatten('#origin#');
+    }
+}
